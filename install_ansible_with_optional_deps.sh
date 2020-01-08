837c80d660a199884a3567686a015ebdae75375a
Dennis Vestergaard Værum <github@varum.dk>
2020-01-08 18:33:47 +0000

    added a simple script to install Ansible and in options deps

---
+++ install_ansible_with_optional_deps.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+pacman -Sy --noconfirm --needed --overwrite='*' ansible
+
+
+optional_deps="$(pacman -Q --info ansible | sed -E -e '1,/^(^Depends On)/d' -e '/^Required By/,$ d'  -e 's/^([^ ][^:]+: |[ ]+)//' -e 's/:.*//')"
+pacman -Sy --noconfirm --needed --overwrite='*' $optional_deps
