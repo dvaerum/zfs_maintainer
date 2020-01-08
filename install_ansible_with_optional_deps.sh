2b3f087f8b0324ec990c6100b76659ae516a7ed4
Dennis Vestergaard Værum <github@varum.dk>
2020-01-08 20:25:07 +0000

    install python in-case it is not the newest version

--- install_ansible_with_optional_deps.sh
+++ install_ansible_with_optional_deps.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-pacman -Sy --noconfirm --needed --overwrite='*' ansible
+pacman -Sy --noconfirm python
 
+pacman -Sy --noconfirm --needed --overwrite='*' ansible
 
 optional_deps="$(pacman -Q --info ansible | sed -E -e '1,/^(^Depends On)/d' -e '/^Required By/,$ d'  -e 's/^([^ ][^:]+: |[ ]+)//' -e 's/:.*//')"
 pacman -Sy --noconfirm --needed --overwrite='*' $optional_deps
+
+yes | pacman -Scc
